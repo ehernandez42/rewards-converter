3df93727026aa317c7501b5a7bcfee305366371f
Eleazar <eleazarfhernandez@gmail.com>
2024-03-14 21:51:29 -0500

    task Two

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,17 +1,25 @@
-// My solution for Task One
 public class RewardValue {
     private double cashValue;
-    private double milesValue = 0.0035;
+    public static final double CASH_TO_MILES_CONVERSION = 0.0035;
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
+    }
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
 
     public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue() {
-        double result = getCashValue() * milesValue;
-        return result;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / CASH_TO_MILES_CONVERSION);
+    }
+    private static double convertToCash(int milesValue) {
+        return milesValue * CASH_TO_MILES_CONVERSION;
     }
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cash = 1000;
+        int expectedResult = (int) (cash / RewardValue.CASH_TO_MILES_CONVERSION);
+        var rewardValue = new RewardValue(cash);
+        assertEquals(expectedResult, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int testMiles = 10000;
+        double expectedCashResult = testMiles * RewardValue.CASH_TO_MILES_CONVERSION;
+        var rewardValue = new RewardValue(testMiles);
+        assertEquals(expectedCashResult, rewardValue.getCashValue());
     }
 }
